0547972f7a9cc14138e42972bed4bcdf4448159f
PortelaMath <matheus.camargo.portela@escola.pr.gov.br>
2025-07-03 11:40:34 -0300

    Add files via upload

--- PalavrasJE25/script.js
+++ PalavrasJE25/script.js
@@ -5,8 +5,28 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
 
 
-    campoResultado.textContent = palavras.join(', ');
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+
+    let frequencias = [];
+    for(let i in palavras) {
+        frequencias [i] = 0;
+        for (let j in palavras) {
+            if (palavras[i]==palavras[j]){
+                frequencias[i]++;
+            }
+    }
+}
+    
+    console.log(frequencias)
+    
+
+
+    return palavras;
 }
